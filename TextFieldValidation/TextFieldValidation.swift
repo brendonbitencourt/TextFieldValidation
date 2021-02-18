5256fb621265f615ed9bd8a5106030c7fa42b44f
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2021-02-17 21:00:23 -0300

    Buscar mensagem de erro via plist

--- TextFieldValidation/TextFieldValidation.swift
+++ TextFieldValidation/TextFieldValidation.swift
@@ -21,6 +21,38 @@ enum TextFieldValidationType {
                 return TextFieldValidationFunctions.maxCharacters(text, num)
         }
     }
+    
+    func getMessage() -> String? {
+        return self.getMessageModel()?.message
+    }
+    
+    func getShortMessage() -> String? {
+        return self.getMessageModel()?.shortMessage
+    }
+    
+    private func getMessageModel() -> TextFieldValidationMessages? {
+        switch self {
+            case .email:
+                return self.getInfoFromPlist(key: "email")
+            case .maxCharacters(num: _):
+                return self.getInfoFromPlist(key: "maxCharacters")
+        }
+    }
+    
+    private func getInfoFromPlist(key: String) -> TextFieldValidationMessages? {
+        guard
+            let path = Bundle.main.path(forResource: "Validations", ofType: "plist"),
+            let xml = FileManager.default.contents(atPath: path)
+        else { return nil }
+        
+        do {
+            let preferences = try PropertyListDecoder().decode([String:TextFieldValidationMessages].self, from: xml)
+            return preferences[key]
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 }
 
 //MARK: - Model
@@ -33,6 +65,11 @@ struct TextFieldValidationError {
 struct TextFieldValidationModel {
     let textField: UITextField?
     let validations: [TextFieldValidationType]?
+}
+
+struct TextFieldValidationMessages: Codable {
+    let message: String?
+    let shortMessage: String?
 }
 
 //MARK: - Delegate
@@ -78,25 +115,27 @@ class TextFieldValidation {
 class TextFieldValidationFunctions {
     static func email(_ text: String?) -> TextFieldValidationError? {
         
+        let error = TextFieldValidationType.email
         let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let predicate = NSPredicate(format:"SELF MATCHES %@", regEx)
         let isValid = predicate.evaluate(with: text)
         
         if !isValid {
-            return TextFieldValidationError(message: "Email inválido", shortMessage: "Inválido", type: .email)
+            return TextFieldValidationError(message: error.getMessage(), shortMessage: error.getShortMessage(), type: error)
         }
         
         return nil
     }
     
-    static func maxCharacters(_ text: String?, _ num: Int) -> TextFieldValidationError? {
+    static func maxCharacters(_ text: String?, _ number: Int) -> TextFieldValidationError? {
         
-        let regEx = "^.{0,\(String(num))}$"
+        let error = TextFieldValidationType.maxCharacters(num: number)
+        let regEx = "^.{0,\(String(number))}$"
         let predicate = NSPredicate(format:"SELF MATCHES %@", regEx)
         let isValid = predicate.evaluate(with: text)
         
         if !isValid {
-            return TextFieldValidationError(message: "Campo inválido", shortMessage: "Inválido", type: .email)
+            return TextFieldValidationError(message: error.getMessage(), shortMessage: error.getShortMessage(), type: error)
         }
         
         return nil
